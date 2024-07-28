8ea51013d91f742422259d27bf78786762198c2f
lafinsd <103396802+lafinsd@users.noreply.github.com>
2024-07-28 12:20:16 -0700

    Initial Commit

---
+++ ASRecNames/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ASRecNames
+//
+//  Created by Larry on 7/28/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
